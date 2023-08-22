0da407cf2ccf77f20e4fc207d25c4945813a1d9b
Seo Hui <seoh7dev@gmail.com>
2023-08-22 12:13:23 +0900

    #3.3-3.4 response

--- src/server.js
+++ src/server.js
@@ -9,8 +9,16 @@ const app = express(); // application을 만들기
 
 // app.get("어떤 route", callback 함수) 이때, callback은 반드시 함수로 보내야 한다.
 
-const handleHome = () => console.log("Somebody is trying to go home.");
+// express의 route handler에서는 req, res object를 받아올 수 있다. (바닐라JS에서 event를 받아왔던 것처럼)
+const handleHome = (req, res) => {
+  return res.end(); // request를 종료하는 응답을 return
+};
+
+const handleLogin = (req, res) => {
+  return res.send("<h1>Login here.</h1>"); // 해당 내용을 화면에 보내는 응답을 return
+};
 app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 // 위의 내용을 app.get("/", () => console.log("Somebody is trying to go home.")); 로 적는 것도 가능하다.
 
