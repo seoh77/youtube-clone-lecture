892065c94fbd619af2d085a433bd542ac2ef299b
Seo Hui <seoh7dev@gmail.com>
2023-08-24 13:36:53 +0900

    #4.0-4.1 Router basic

--- src/server.js
+++ src/server.js
@@ -1,25 +1,28 @@
 import express from "express";
-import morgan from "morgan"; // "morgan"으로 부터 import만 해온다면 이름은 뭐로 설정하든 상관없다.
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
 
-const home = (req, res) => {
-  console.log("I will respond.");
-  return res.send("hello");
-};
-
-const login = (req, res) => {
-  return res.send("login");
-};
-
 app.use(logger);
-//  app.use(morgan("dev")) 이런 식으로 따로 변수를 선언하지 않고 한 번에 적어 사용할 수도 있다.
 
-app.get("/", home);
-app.get("/login", login);
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter); // 누군가가 "/videos"로 시작하는 url에 접근하면 videoRouter에 있는 컨트롤러를 찾게 하는 역할
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
