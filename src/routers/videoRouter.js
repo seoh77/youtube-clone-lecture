ada45bedf44837709e8b59d29a219d54ba9c30df
Seo Hui <seoh7dev@gmail.com>
2023-08-24 16:35:10 +0900

    #4.7 Parameter

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,7 +3,7 @@ import { upload, see, edit, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
+videoRouter.get("/upload", upload); // upload가 :id보다 아래에 있으면 'upload'를 :id의 값이라고 판단해버린다.
 videoRouter.get("/:id", see);
 videoRouter.get("/:id/edit", edit);
 videoRouter.get("/:id/delete", deleteVideo);
