ccbc3c4e2dcd201b1d6426bacfbea813e379c3d4
Seo Hui <seoh7dev@gmail.com>
2023-08-28 15:16:13 +0900

    #5.7 Conditionals

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,10 @@
-// res.render("veiw 이름", "템플릿에 보낼 변수")
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+const fakeUser = {
+  username: "S",
+  loggedIn: false,
+};
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", fakeUser: fakeUser });
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
