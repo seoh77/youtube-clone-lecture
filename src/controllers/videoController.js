815cdc5e2fcd0345ac0d2d8cb47f4646df85434a
Seo Hui <seoh7dev@gmail.com>
2023-08-28 15:28:34 +0900

    #5.8 Iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,8 @@
-const fakeUser = {
-  username: "S",
-  loggedIn: false,
+export const trending = (req, res) => {
+  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // const videos = [];
+  res.render("home", { pageTitle: "Home", videos });
 };
-
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser: fakeUser });
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
