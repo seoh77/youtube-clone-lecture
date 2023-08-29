df9caa711e79ed0ec372b1290ce173bb86bc7e3d
Seo Hui <seoh7dev@gmail.com>
2023-08-29 16:31:15 +0900

    #6.0-6.1 Array Database

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,33 +1,39 @@
+// 진짜 DB를 사용하기 전, 우선 가짜 DB(=Array DB)를 사용해서 방법 익히기
+const videos = [
+  {
+    title: "Video #1",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes age",
+    views: 1,
+    id: 1,
+  },
+  {
+    title: "Video #2",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes age",
+    views: 10,
+    id: 2,
+  },
+  {
+    title: "Video #3",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes age",
+    views: 20,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "Video #1",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes age",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Video #2",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes age",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Video #3",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes age",
-      views: 59,
-      id: 1,
-    },
-  ];
   res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("watch");
+export const watch = (req, res) => {
+  const { id } = req.params; // const id = req.params.id를 ES6를 사용하면 const { id } = req.params 으로 나타낼 수 있다.
+  const video = videos[id - 1]; // 컴퓨터는 0부터 숫자를 세니까 -1을 해줘야 한다. 아니면 아예 videos object에 id값을 0부터 설정해줘도 된다.
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { upload, see, edit, deleteVideo } from "../controllers/videoController";
+import {
+  upload,
+  edit,
+  deleteVideo,
+  watch,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", see); // '\d+'는 id값을 숫자로 제한하는 역할을 한다.
+videoRouter.get("/:id(\\d+)", watch); // '\d+'는 id값을 숫자로 제한하는 역할을 한다.
 videoRouter.get("/:id(\\d+)/edit", edit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 videoRouter.get("/upload", upload); // 정규식을 사용하여 이젠 :id 아래에 /upload가 있어도 문제없다.
