4f6ea342128340bf7b23c6a6e5e3bfa92d5cdc36
seoh77 <seoh7dev@gmail.com>
2023-09-04 17:02:33 +0900

    #6.20-6.21 Edit Video (1)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -10,23 +10,38 @@ export const watch = async (req, res) => {
   // findOne : 입력한 모든 조건을 적용시켜서 조건을 충족하는 요소를 찾을 수 있다.  -> 예를 들어 조회수가 25인 영상을 찾아라
   // findById : id로 해당 요소를 찾아낼 수 있는 기능
   const video = await Video.findById(id);
-  if (video) {
-    return res.render("watch", { pageTitle: video.title, video });
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+    // video가 null이면 여기서 실행을 멈춰야 하니끼 꼭 앞에 return을 붙여야 한다. 안 그러면 그 밑에 코드들도 이어서 실행시킨다.
   }
-  return res.render("404", { pageTitle: "Video not found." });
+  return res.render("watch", { pageTitle: video.title, video });
 };
 
 // getEdit : 화면에 보여주는 역할
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  return res.render("edit", { pageTitle: `Editing` });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
 
 // postEdit : 변경사항을 저장해주는 역할
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body; // const title = req.body.title 과 같은 의미
-  return res.redirect(`/videos/${id}`); // 브라우저가 설정한 주소로 redirect(자동으로 이동)된다.
+  const { title, description, hashtags } = req.body;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  video.title = title;
+  video.description = description;
+  video.hashtags = hashtags
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`)); // 만약 단어가 #으로 시작한다면 단어 그대로 return하고, #으로 시작하지 않을 때만 앞에 #을 붙여준다.
+  await video.save();
+  return res.redirect(`/videos/${id}`);
 };
 
 export const getUpload = (req, res) => {
