e766066b14043c6047c062bcef4708f56835f5fe
seoh77 <seoh7dev@gmail.com>
2023-09-04 14:07:22 +0900

    #6.13 Model.find() promise

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,10 @@
 import Video from "../models/Video";
 
-export const home = (req, res) => {
-  // callback function을 사용하는 경우  <- 하지만 현 시점(2023.9) 기준 model.find()에서 콜백함수를 사용하지 못하도록 몽구스 업데이트가 되었음. 따라서 작동되진 않음
-  Video.find({}, (error, videos) => {
-    return res.render("home", { pageTitle: "Home", videos: [] });
-  });
+export const home = async (req, res) => {
+  const videos = await Video.find({});
+  return res.render("home", { pageTitle: "Home", videos });
 };
+
 export const watch = (req, res) => {
   const { id } = req.params; // const id = req.params.id를 ES6를 사용하면 const { id } = req.params 으로 나타낼 수 있다.
   return res.render("watch", { pageTitle: `Watching` });
