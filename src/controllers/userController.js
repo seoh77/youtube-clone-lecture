3bfb557bace3a6c342b17d7603e645606b8d21a3
seoh77 <seoh7dev@gmail.com>
2023-09-05 14:21:04 +0900

    #7.3 Form Validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,8 +2,26 @@ import User from "../models/User";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
-  console.log(req.body);
-  const { name, username, email, password, location } = req.body;
+  const { name, username, email, password, password2, location } = req.body;
+  const pageTitle = "Join";
+
+  // 비밀번호 확인을 위해 비밀번호를 두 번 입력하고, 그 두 개의 값이 같은지 확인
+  if (password !== password2) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "Password confirmaiton does not match.",
+    });
+  }
+
+  // 이미 사용 중인 username 또는 email을 체크하는 작업
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "This username/email is already taken.",
+    });
+  }
+
   await User.create({
     name,
     username,
