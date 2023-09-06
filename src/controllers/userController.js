5f34b57ac6899612676278710691ee76f14ab7c5
seoh77 <seoh7dev@gmail.com>
2023-09-06 16:54:26 +0900

    #7.22 Log Out

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -149,6 +149,8 @@ export const finishGithubLogin = async (req, res) => {
 };
 
 export const edit = (req, res) => res.send("Edit User");
-export const remove = (req, res) => res.send("Remove User");
-export const logout = (req, res) => res.send("Logout");
+export const logout = (req, res) => {
+  req.session.destroy();
+  return res.redirect("/");
+};
 export const see = (req, res) => res.send("See User");
