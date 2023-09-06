052b82f6ba27a7c46c8d8dce0650c400570787d0
seoh77 <seoh7dev@gmail.com>
2023-09-06 21:00:21 +0900

    #8.1 Protector and Public Middleware

--- src/middlewares.js
+++ src/middlewares.js
@@ -4,3 +4,21 @@ export const localMiddleware = (req, res, next) => {
   res.locals.loggedInUser = req.session.user || {};
   next();
 };
+
+// 로그인하지 않은 user가 로그인해야 접근할 수 있는 URL에 접근하는 것을 막기 위한 middleware
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+
+// 이미 로그인 한 user가 로그인 페이지와 같이 로그인하지 않은 사람들을 위한 URL에 접근하는 것을 막기 위한 middleware
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
+};
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -7,13 +7,14 @@ import {
   startGithubLogin,
   finishGithubLogin,
 } from "../controllers/userController";
+import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout);
-userRouter.route("/edit").get(getEdit).post(postEdit);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
+userRouter.get("/logout", protectorMiddleware, logout);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit); // all(미들웨어명)을 사용하면 get, post 등 어떤 HTTP method를 사용하든지 해당 미들웨어를 사용하겠다는 것을 의미한다.
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get(":id", see);
 
 export default userRouter;
