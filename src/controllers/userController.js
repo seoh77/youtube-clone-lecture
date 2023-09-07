45469ce40b14a642665b9a54fc9aea67ff308afe
seoh77 <seoh7dev@gmail.com>
2023-09-07 19:47:44 +0900

    #8.12 User's Videos (1)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,4 @@
+import Video from "../models/Video";
 import User from "../models/User";
 import fetch from "node-fetch";
 import bcrypt from "bcrypt";
@@ -223,5 +224,6 @@ export const see = async (req, res) => {
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User not found." });
   }
-  return res.render("users/profile", { pageTitle: user.name, user });
+  const videos = await Video.find({ owner: user._id });
+  return res.render("users/profile", { pageTitle: user.name, user, videos });
 };
