3d5129ba9e4e4d60c8c390feacd8c01d4fe22650
seoh77 <seoh7dev@gmail.com>
2023-09-15 17:06:05 +0900

    #13.4 Downloading the File

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -3,11 +3,20 @@ const video = document.getElementById("preview");
 
 let stream;
 let recorder;
+let videoFile;
+
+const handleDownload = () => {
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "MyRecording.webm"; // 다운로드 할 포맷도 지정해주기
+  document.body.appendChild(a);
+  a.click();
+};
 
 const handleStop = () => {
-  startBtn.innerText = "Start Recording";
+  startBtn.innerText = "Download Recording";
   startBtn.removeEventListener("click", handleStop);
-  startBtn.addEventListener("click", handelStart);
+  startBtn.addEventListener("click", handleDownload);
   recorder.stop();
 };
 
@@ -20,7 +29,7 @@ const handelStart = () => {
 
   recorder = new MediaRecorder(stream);
   recorder.ondataavailable = (event) => {
-    const videoFile = URL.createObjectURL(event.data); // createObjectURL은 브라우저 메모리에서만 가능한 URL을 만들어준다.
+    videoFile = URL.createObjectURL(event.data); // createObjectURL은 브라우저 메모리에서만 가능한 URL을 만들어준다.
     video.srcObject = null;
     video.src = videoFile; // 녹화한 비디오 미리보기
     video.loop = true; // 반복재생
